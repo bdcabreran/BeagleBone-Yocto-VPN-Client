0afcbc43184d9742025c53ba54f432133c3d495c
bdcabreran <bdcabreran@unal.edu.co>
2024-03-15 17:00:15 +0000

    feat : Adding vpn scripts for client

---
+++ sources/meta-custom/recipes-custom/openvpn-scripts/files/start-vpn-client.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Function to check internet connectivity
+check_internet() {
+    wget -q --spider http://google.com
+
+    if [ $? -eq 0 ]; then
+        echo "Internet is connected."
+        return 0
+    else
+        echo "No internet connection."
+        return 1
+    fi
+}
+
+# Main script execution starts here
+if check_internet; then
+    echo "Loading tun module..."
+    modprobe tun
+
+    echo "Executing update-time script..."
+    /usr/bin/update-time.sh
+
+    echo "Starting OpenVPN..."
+    openvpn --config /usr/bin/client1.opvn
+else
+    echo "Skipping operations due to lack of internet connectivity."
+fi
